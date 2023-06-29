0864bd2590e2ec716fb8e30123e87ebc0b58a66e
Keenan Smith <keenan.smith@uconn.edu>
2023-06-29 16:29:47 -0400

    Feature: Create base classes for TDAmeritradeClient
    Adds TDAmeritradeClient.cs and all supporting classes/interfaces for it

---
+++ src/TDAmeritrade/BaseClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+
+namespace TDAmeritrade
+{
+    /// <summary>
+    /// The base interface for interacting with TD Ameritrade's API.
+    /// </summary>
+    public abstract class BaseClient : ITDAmeritradeClient
+    {
+        private const string ContentType = "Content-Type";
+        private const string DefaultMediaType = "application/json";
+
+        /// <summary>
+        /// The client assembly version to send in request User-Agent header.
+        /// </summary>
+        private static readonly string ClientVersion = typeof(BaseClient).GetTypeInfo().Assembly.GetName().Version.ToString();
+
+        /// <summary>
+        /// The configuration to use with current client instance.
+        /// </summary>
+        private readonly BaseClientOptions options;
+
+        /// <summary>
+        /// The HttpClient instance to use for all calls from this client instance.
+        /// </summary>
+        private readonly HttpClient client;
+
+        /// <summary>
+        /// The path to the API resource.
+        /// </summary>
+        public string UrlPath
+        {
+            get => this.options.UrlPath;
+            set => this.options.UrlPath = value;
+        }
+
+        /// <summary>
+        /// The API version.
+        /// </summary>
+        public string Version
+        {
+            get => this.options.Version;
+            set => this.options.Version = value;
+        }
+    }
+}
---
+++ src/TDAmeritrade/ITDAmeritradeClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TDAmeritrade
+{
+    /// <summary>
+    /// An HTTP client wrapper for interacting with TD Ameritrade's API.
+    /// </summary>
+    public interface ITDAmeritradeClient
+    {
+        /// <summary>
+        /// Gets or sets the path to the API resource.
+        /// </summary>
+        string UrlPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the API version.
+        /// </summary>
+        string Version { get; set; }
+    }
+}
